fd2e90e7e26b6cc11b902a01dd7c9b59ac67e090
Ruth <mensirer@gmail.com>
2022-10-12 11:35:54 -0400

    bubble sort betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,25 @@
- /**
- * File: 0-bubble_sort.c
- */
-
 #include "sort.h"
-
 /**
- * swap_ints - Swap two integers in the array.
- * @num1: The first integer to swap.
- * @num2: The second integer to swap.
- */
-void swap_ints(int *num1, int *num2)
-{
-	int tmp;
-
-	tmp = *num1;
-	*num1 = *num2;	*num2 = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- */
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i, j, n;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (bubbly == false)
+	for (i = 0; i < size; i++)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j - 1] > array[j])
 			{
-				swap_ints(array + i, array + i + 1);
+				n = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = n;
 				print_array(array, size);
-				bubbly = false;
 			}
 		}
-		len--;
 	}
 }
